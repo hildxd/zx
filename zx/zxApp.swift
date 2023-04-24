6aa0dc1bfdb1fa1490d49ab5cd1007c86e384ef7
sunlee <hildxd@outlook.com>
2023-04-24 21:29:05 +0800

    Initial Commit

---
+++ zx/zxApp.swift
@@ -0,0 +1,17 @@
+//
+//  zxApp.swift
+//  zx
+//
+//  Created by 李旭东 on 2023/4/24.
+//
+
+import SwiftUI
+
+@main
+struct zxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
